dd62eddea75d8c75301ac5fc4ea87c9c2dc48aa1
First Second <v1adp1exu5@gmail.com>
2019-08-31 14:23:49 +0300

    add NumberWrittenTranslator

---
+++ Code/Agbm.Helpers/NumberWrittenTranslator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agbm.Helpers
+{
+    public static class NumberWrittenTranslator
+    {
+        private const string ZERO = "No";
+
+        private static readonly string[] _ones = new[] { "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
+        public static string Translate( this int num )
+        {
+            if ( num == 0 ) {
+                return ZERO;
+            }
+
+            return GetLiteral( num, new StringBuilder() );
+        }
+
+        private static string GetLiteral( int num, StringBuilder sb )
+        {
+            if ( num < 10 ) {
+                sb.Append( _ones[ num ] );
+            }
+
+            return sb.ToString();
+        }
+    }
+}
